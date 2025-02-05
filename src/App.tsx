13889f3b303ff65816bce9594ed76cde16b43f52
Eddie Zaldivar <EddieZal28@gmail.com>
2025-02-05 13:23:18 -0800

    Still no fix, tiles aren't showing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ComingSoon from "./components/ComingSoon/ComingSoon";
-import Homegrown from "./components/Homegrown/HomeGrown";
+import Homegrown from "./components/Homegrown/Homegrown";
 
 const App: React.FC = () => {
   return (
     <>
       <ComingSoon />
-      <HomegrownSection />
-      <Homegrown title="Locally Grown Goodness" description="Discover"
-
+      <Homegrown
+        title="Locally Grown Goodness"
+        description="Discover fresh, locally sourced products."
+        ctaLabel="Learn More"
+        onCtaClick={() => alert("CTA Clicked!")}
+      />
     </>
   );
 };
